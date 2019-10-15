880c0b59b3ee7695d5738c561624b3de9d1b9daf
Michael Cabana <m.cabana@ieg-america.com>
2019-10-15 17:18:03 -0400

    Initial commit

---
+++ src/components/layout/layout.js
@@ -0,0 +1,5 @@
+const Layout = (props) => {
+    return <div className="layout"></div>
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            members: [],
+        }
+    }
+
+
+
+    render() {
+        <div>
+            <h3>Welcome to Next.js!</h3>
+            <img src="/view.jpg" alt="image" style={{ width: '25vw' }} />
+        </div>
+    }
+}
+
+export default Home
